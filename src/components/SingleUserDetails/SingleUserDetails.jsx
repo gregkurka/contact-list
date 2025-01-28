e4f4ac37da30aa0fd2e050a8c8f3910e8a7848b0
Greg Kurka <gregkurka@gmail.com>
2025-01-28 11:58:50 -0600

    finished up according to project details

--- src/components/SingleUserDetails/SingleUserDetails.jsx
+++ src/components/SingleUserDetails/SingleUserDetails.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function SingleUserDetails({ setFeaturedUser }) {
+function SingleUserDetails({ setFeaturedUser, featuredUser }) {
+  const [userDetails, setUserDetails] = useState(null);
+  useEffect(() => {
+    axios(
+      `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/${featuredUser}`
+    )
+      .then((data) => setUserDetails(data.data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <div>
-      SingleUserDetails
+    <div className="single-details">
+      <h2>Details for {userDetails?.name}</h2>
+      <p>Phone: {userDetails?.phone}</p>
+      <p>Email: {userDetails?.email}</p>
+      <p>
+        Address: {userDetails?.address?.street}
+        {", "} {userDetails?.address?.city} {userDetails?.address?.zipcode}
+      </p>
+
       <button onClick={() => setFeaturedUser(null)}>Go back to list</button>
     </div>
   );
